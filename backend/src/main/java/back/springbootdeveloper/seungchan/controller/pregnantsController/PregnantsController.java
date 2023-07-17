38a0c6796241e658a5669ec4bb2475c2f5f464ab
dmdrk1414 <seungchan141414@gmail.com>
2023-07-18 00:33:53 +0900

    [ADD] 임산부 각 게시물을 불러온다.

---
+++ backend/src/main/java/back/springbootdeveloper/seungchan/controller/pregnantsController/PregnantsController.java
@@ -0,0 +1,28 @@
+package back.springbootdeveloper.seungchan.controller.pregnantsController;
+
+import back.springbootdeveloper.seungchan.domain.Article;
+import back.springbootdeveloper.seungchan.dto.EachArticleResponseDTO;
+import back.springbootdeveloper.seungchan.service.oldmansDisablesService.OldmansDisablesService;
+import back.springbootdeveloper.seungchan.service.pregnantsService.PregnantsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor // final 이 붙거나 @NotNull 이 붙은 필드의 생성자를 자동 생성해주는 룸복 어노테이션
+@RestController
+public class PregnantsController {
+    private final PregnantsService pregnantsService;
+
+    // 박승찬
+    @GetMapping("/article/pregnants/{id}")
+    // @PathVariable 매너테이션은 URL에서 값을 가져오는 애너테이션
+    // /api/articles/3 GET 요청을 받으면 id에 3이 들어온다.
+    public ResponseEntity<EachArticleResponseDTO> findArticleById(@PathVariable long id) {
+        Article article = pregnantsService.findById(id);
+
+        return ResponseEntity.ok()
+                .body(new EachArticleResponseDTO(article));
+    }
+}
