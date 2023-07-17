330a0970ea228e3f1c32220676c7a30d4f77a81d
dmdrk1414 <seungchan141414@gmail.com>
2023-07-18 00:33:36 +0900

    [ADD] 노인/장애인의 각 게시물을 불러온다.

---
+++ backend/src/main/java/back/springbootdeveloper/seungchan/service/oldmansDisablesService/OldmansDisablesService.java
@@ -0,0 +1,23 @@
+package back.springbootdeveloper.seungchan.service.oldmansDisablesService;
+
+import back.springbootdeveloper.seungchan.domain.Article;
+import back.springbootdeveloper.seungchan.util.Util;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor // final이 붙거나  @NotNull이 붙는 필드의 생성자 추가
+@Service  // 해당 클래스를 빈으로 서블릿 컨테이너에 등록
+public class OldmansDisablesService {
+    public Article findById(long id) {
+        List<Article> list = Util.getTempList();
+
+        for (Article article : list) {
+            if (id == article.getId()) {
+                return article;
+            }
+        }
+        throw new IllegalArgumentException("각각의 게시물 예외 처리");
+    }
+}
